b776e850bec362eb52f177ae6bf4b1d73fd91232
marwa-2025 <marawsuliman@gmail.com>
2024-12-31 15:49:50 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,328 @@
+<?php
+include('inc/head.php');
+
+include('inc/nav.php')
+?> 
+<section class="hero-wrap hero-wrap-2" style="background-image: url(' <?php echo site_url?>theme/images/bg_2.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-end justify-content-center">
+        <div class="col-md-9 ftco-animate pb-5 text-center">
+          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+                  class="fa fa-chevron-right"></i></a></span> <span>About us <i class="fa fa-chevron-right"></i></span>
+          </p>
+          <h1 class="mb-0 bread">About Us</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+  <section class="ftco-section ftco-about img">
+    <div class="container">
+      <div class="row d-flex">
+        <div class="col-md-12 about-intro">
+          <div class="row">
+            <div class="col-md-6 d-flex">
+              <div class="d-flex about-wrap">
+                <div class="img d-flex align-items-center justify-content-center"
+                  style="background-image:url(<?php echo site_url?>theme/images/about-1.jpg);">
+                </div>
+                <div class="img-2 d-flex align-items-center justify-content-center"
+                  style="background-image:url(<?php echo site_url?>theme/images/about.jpg);">
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 pl-md-5 py-5">
+              <div class="row justify-content-start pb-3">
+                <div class="col-md-12 heading-section ftco-animate">
+                  <span class="subheading">Shaping Future Leaders</span>
+                  <h2 class="mb-4">Life and Career Ready</h2>
+                  <p>Since our founding in 2024. We pride ourselves on offering a wide array of graduate programs,
+                    including Master's and PhD degrees, tailored to equip our students with the expertise and skills
+                    needed to excel in their chosen fields, we aim to shape the leaders of tomorrow by providing
+                    comprehensive and holistic educational experience</p>
+                  <p><a href="Course.html" class="btn btn-primary">Show Degrees</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(<?php echo site_url?>theme/images/bg_4.jpg);">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+          <div class="block-18 d-flex align-items-center">
+            <div class="icon"><span class="flaticon-online"></span></div>
+            <div class="text">
+              <strong class="number" data-number="400">0</strong>
+              <span>Online Courses</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+          <div class="block-18 d-flex align-items-center">
+            <div class="icon"><span class="flaticon-graduated"></span></div>
+            <div class="text">
+              <strong class="number" data-number="4500">0</strong>
+              <span>Students Enrolled</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+          <div class="block-18 d-flex align-items-center">
+            <div class="icon"><span class="flaticon-instructor"></span></div>
+            <div class="text">
+              <strong class="number" data-number="1200">0</strong>
+              <span>Experts Instructors</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+          <div class="block-18 d-flex align-items-center">
+            <div class="icon"><span class="flaticon-tools"></span></div>
+            <div class="text">
+              <strong class="number" data-number="300">0</strong>
+              <span>Hours Content</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="ftco-section testimony-section bg-light">
+    <div class="overlay" style="background-image: url(<?php echo site_url ?>theme/images/bg_2.jpg);"></div>
+    <div class="container">
+      <div class="row pb-4">
+        <div class="col-md-7 heading-section ftco-animate">
+          <span class="subheading">Testimonial</span>
+          <h2 class="mb-4">What Our Students Say</h2>
+        </div>
+      </div>
+    </div>
+    <div class="container container-2">
+      <div class="row ftco-animate">
+        <div class="col-md-12">
+          <div class="carousel-testimony owl-carousel">
+            <div class="item">
+              <div class="testimony-wrap py-4">
+                <div class="text">
+                  <p class="star">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star-half-o"></span>
+                    <span class="fa fa-star-o"></span>
+                  </p>
+                  <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts.</p>
+                  <div class="d-flex align-items-center">
+                    <div class="user-img" style="background-image: url(<?php echo site_url?>theme/images/person_1.jpg)"></div>
+                    <div class="pl-3">
+                      <p class="name">Fawzia Mohammed </p>
+                      <span class="position">Mental health</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="testimony-wrap py-4">
+                <div class="text">
+                  <p class="star">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star-half-o"></span>
+                    <span class="fa fa-star-o"></span>
+                  </p>
+                  <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts.</p>
+                  <div class="d-flex align-items-center">
+                    <div class="user-img" style="background-image: url(<?php echo site_url?>theme/images/person_2.jpg)"></div>
+                    <div class="pl-3">
+                      <p class="name">Widad Ahmed </p>
+                      <span class="position">professional doctorate</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="testimony-wrap py-4">
+                <div class="text">
+                  <p class="star">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star-half-o"></span>
+                    <span class="fa fa-star-o"></span>
+                  </p>
+                  <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts.</p>
+                  <div class="d-flex align-items-center">
+                    <div class="user-img" style="background-image: url(<?php echo site_url?>theme/images/person_3.jpg)"></div>
+                    <div class="pl-3">
+                      <p class="name">Ayman Al-Thaqafi</p>
+                      <span class="position">Marketing Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="testimony-wrap py-4">
+                <div class="text">
+                  <p class="star">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star-half-o"></span>
+                    <span class="fa fa-star-o"></span>
+                  </p>
+                  <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts.</p>
+                  <div class="d-flex align-items-center">
+                    <div class="user-img" style="background-image: url(<?php echo site_url?>theme/images/person_1.jpg)"></div>
+                    <div class="pl-3">
+                      <p class="name">Ali Ibrahim</p>
+                      <span class="position">Marketing Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="testimony-wrap py-4">
+                <div class="text">
+                  <p class="star">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star-half-o"></span>
+                    <span class="fa fa-star-o"></span>
+                  </p>
+                  <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts.</p>
+                  <div class="d-flex align-items-center">
+                    <div class="user-img" style="background-image: url(<?php echo site_url?>theme/images/person_2.jpg)"></div>
+                    <div class="pl-3">
+                      <p class="name">Roger Scott</p>
+                      <span class="position">Marketing Manager</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-intro ftco-section ftco-no-pb">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-12 text-center">
+          <div class="img" style="background-image: url(<?php echo site_url?>theme/images/bg_4.jpg);">
+            <div class="overlay"></div>
+            <h2>Verify Your Credentials</h2>
+            <p>Verify the legitimacy of your awarded certificate with our simple and quick verification process.</p>
+            <p class="mb-0"><a href="instructor.html" class="btn btn-primary px-4 py-3">Check Now</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-section services-section">
+    <div class="container">
+      <div class="row d-flex">
+        <div class="col-md-6 heading-section pr-md-5 ftco-animate d-flex align-items-center">
+          <div class="w-100 mb-4 mb-md-0">
+            <span class="subheading">Welcome to Infinity University</span>
+            <h2 class="mb-4">Unlock Your Potential With Us</h2>
+            <p>Join us to discover the transformative power of education and unlock your full potential.
+              Our comprehensive support system ensures you have the guidance and assistance you need every step of the
+              way. Engage in diverse activities that complement your academic pursuits, and build a network that lasts a
+              lifetime.
+            </p>
+            <!-- <div class="d-flex video-image align-items-center mt-md-4">
+              <a href="#" class="video img d-flex align-items-center justify-content-center"
+                style="background-image: url(images/about.jpg);">
+                <span class="fa fa-play-circle"></span>
+              </a>
+              <h4 class="ml-4">Learn anything from Infinity University, Watch video</h4>
+            </div> -->
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate">
+              <div class="services">
+                <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-tools"></span>
+                </div>
+                <div class="media-body">
+                  <h3 class="heading mb-3">Top-Quality Education Worldwide</h3>
+                  <p>We offer modern, internationally approved programs that will help you build
+                    skills and grow in your career</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate">
+              <div class="services">
+                <div class="icon icon-2 d-flex align-items-center justify-content-center"><span
+                    class="flaticon-instructor"></span></div>
+                <div class="media-body">
+                  <h3 class="heading mb-3">A Fresh Approach to Education</h3>
+                  <p>Discover new and exciting ways to learn using the latest technology and
+                    top-quality academic materials for a more engaging experience</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate">
+              <div class="services">
+                <div class="icon icon-3 d-flex align-items-center justify-content-center"><span
+                    class="flaticon-quiz"></span></div>
+                <div class="media-body">
+                  <h3 class="heading mb-3">Learn Anytime &amp; Anywhere</h3>
+                  <p>Get unlimited access to your courses materials and resources whenever you need
+                    them, so you can study at your own pace and fit learning into your schedule</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate">
+              <div class="services">
+                <div class="icon icon-4 d-flex align-items-center justify-content-center"><span
+                    class="flaticon-browser"></span></div>
+                <div class="media-body">
+                  <h3 class="heading mb-3">Ongoing Support from Experts</h3>
+                  <p>Receive personal support from experienced advisors and trainers who are here to
+                    guide you and help you achieve your educational and career goals.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php
+include('inc/footer.php');
+
+
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include('inc/head.php');
+
+
+?>
+
+   <?php include('inc/nav.php') ?>
+    <!-- END nav -->
+    <?php include('inc/hero.php') ?>
+
+ <!-- END hero -->
+   
+    <?php include('inc/register_form.php') ?>
+
+ <!-- END register_form -->
+ <?php include('inc/start_learn.php') ?>
+
+ <!-- END start_learn -->
+ <?php include('inc/services.php') ?>
+
+ <!-- END services -->
+ <?php include('inc/light.php') ?>
+
+    <!-- END light -->
+    <?php include('inc/counter.php') ?>
+
+    <!-- END counter -->
+    <?php include('inc/about.php') ?>
+
+    <!-- END about -->
+    <?php include('inc/intro.php') ?>
+    <!-- END intro -->
+    <?php include('inc/testimony.php')?>
+        <!-- END testimony -->
+
+    <?php
+include('inc/footer.php');
+
+    ?>
+   
